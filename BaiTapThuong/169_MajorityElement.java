c48d2811a6497bb77a4297724bb9cd15d1d04ca4
Phuc Hieu <66401141+trinhphuchieu@users.noreply.github.com>
2021-11-23 23:21:18 +0700

    Update 169_MajorityElement.java

--- BaiTapThuong/169_MajorityElement.java
+++ BaiTapThuong/169_MajorityElement.java
@@ -1,9 +1,10 @@
 import java.util.HashMap;
 
 
-// chua toi uu :((
+
 
 class Solution {
+    // chua toi uu :((
     public int majorityElement(int[] nums) {
        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
         for(int i = 0 ; i < nums.length ; i++)
@@ -24,7 +25,25 @@ class Solution {
                 k = key;
             } 
         }
-        return k;
-       
+        return k;    
+    }
+    
+    // A Linear Time Majority Vote Algorithm
+    // Điều kiện áp dụng thuật toán khi số phần tử trong mảng chiếm hơn [n/2]. :( 
+    public int majorityElement(int[] num) {
+        int mj = num[0];
+        int count = 0;
+        for(int i  = 0 ; i < num.length ; i++)
+        {
+            if(count == 0) mj = num[i];
+            if(mj == num[i])
+            {
+                count+=1;
+            }else
+            {
+                count -= 1;    
+            }
+        }
+        return mj;       
     }
 }
