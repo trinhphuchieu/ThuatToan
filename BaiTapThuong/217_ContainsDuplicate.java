2d189771c2f5cc3233ebb1023e86c8739081db05
Phuc Hieu <66401141+trinhphuchieu@users.noreply.github.com>
2021-11-25 14:37:09 +0700

    Create 217_ContainsDuplicate.java

---
+++ BaiTapThuong/217_ContainsDuplicate.java
@@ -0,0 +1,27 @@
+class Solution {
+    
+
+     // way 1 fast Eeasy hiểu
+    
+    public boolean containsDuplicate(int[] nums) {
+        int count = 0;
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length-1; i++)
+        {
+            if(nums[i] == nums[i+1]) return true;    
+        }
+        return false;
+    }
+    
+    
+    //  way 2 slow cách 1 và very điều mới về set java :)
+    public boolean containsDuplicate(int[] nums) {
+        int count = 0;
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for(int i = 0 ; i < nums.length; i++)
+        {
+            if(!hs.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
